00af2a50be294849a186084f075974cc0b7d9827
GimliLongBow <gimlilongbow@gmail.com>
2010-09-13 21:02:54 -0400

    Reverting the RegExp back to an older, not-so-fancy-but-working version.

--- js/script.js
+++ js/script.js
@@ -89,6 +89,6 @@ function deleteNote(e) {
 }
 
 function convertUrls(text) {
-	var exp = new RegExp('[\"\'>]*\b(?:(?:https?|ftp|file)://|www\.|ftp\.)[-A-Z0-9+&@#/%=~_|$?!:,.]*[A-Z0-9+&@#/%=~_|$]');
+	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 	return text.replace(exp,"<a href='$1' target='_blank'>$1</a>"); 
 }
